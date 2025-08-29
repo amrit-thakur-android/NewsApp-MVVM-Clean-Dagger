da09d7db9d7e58706cb935ad6dcd5553f40c0331
amrit-spoton <athakur@spoton.com>
2025-08-29 23:40:44 +0530

    Add navigation related classes.

---
+++ presentation/src/main/kotlin/com/amritthakur/newsapp/presentation/navigation/NavigationCoordinator.kt
@@ -0,0 +1,12 @@
+package com.amritthakur.newsapp.presentation.navigation
+
+import androidx.navigation.NavController
+
+class NavigationCoordinator(
+    private val navController: NavController
+) {
+
+    fun handleNavigationEvent(navigationEvent: NavigationEvent) {
+
+    }
+}
