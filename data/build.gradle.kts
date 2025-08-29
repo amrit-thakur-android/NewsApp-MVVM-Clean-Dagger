e66424f9ee7fd973499b8263835229352981b507
amrit-spoton <athakur@spoton.com>
2025-08-29 12:27:22 +0530

    Add ksp support.

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -53,7 +54,7 @@ dependencies {
     implementation(libs.okhttp.logging)
     implementation(libs.moshi)
     implementation(libs.moshi.kotlin)
-    kapt(libs.moshi.codegen)
+    ksp(libs.moshi.codegen)
 
     implementation(libs.dagger)
 
